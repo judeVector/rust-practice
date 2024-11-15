a2fead0f48c6f4e3ad8378c86bfbd04b0496585b
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-11-16 00:26:44 +0400

    Create slice.rs

---
+++ slice.rs
@@ -0,0 +1,15 @@
+pub mod slice_type {
+
+    #[allow(dead_code)]
+    pub fn first_word(s: &String) -> &str {
+        let bytes = s.as_bytes();
+
+        for (i, &item) in bytes.iter().enumerate() {
+            if item == b' ' {
+                return &s[0..i];
+            }
+        }
+
+        &s[..]
+    }
+}
