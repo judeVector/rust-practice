3c8c84ea04b864d74003cffccf7aee0fba6c76d7
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-11-15 22:20:29 +0400

    Update myimpl.rs

--- myimpl.rs
+++ myimpl.rs
@@ -1,3 +1,5 @@
+// Vehicle Code
+
 #[derive(Debug)]
 #[allow(dead_code)]
 enum VehicleColor {
@@ -31,7 +33,7 @@ impl Vehicle {
         new_vehicle
     }
 }
-
+#[allow(dead_code)]
 fn new_vehicle() -> Vehicle {
     let mut vehicle = Vehicle {
         manaufacturer: "Porsche".to_string(),
@@ -49,4 +51,48 @@ pub fn create_vehicle() {
     println!("{:?}", vehicle);
     vehicle.paint(VehicleColor::Yellow);
     println!("{:?}", vehicle);
+}
+
+// Person Code
+
+struct Person {
+    first_name: String,
+    last_name: String,
+    birth_year: u16,
+    birth_month: u8,
+    visited_europe: bool,
+    meter_walked: u32,
+}
+
+impl Person {
+    fn walk(&mut self, meters: u32) {
+        self.meter_walked += meters
+    }
+}
+
+fn new_person() -> Person {
+    let person1: Person = Person {
+        first_name: "Jude".to_string(),
+        last_name: "Ndubuisi".to_string(),
+        birth_year: 1998,
+        birth_month: 3,
+        visited_europe: true,
+        meter_walked: 0,
+    };
+    person1
+}
+
+pub fn create_person() {
+    let mut person: Person = new_person();
+    person.walk(8);
+    person.walk(18);
+    println!(
+        "first name: {}, last name: {}, birth year: {}, birth month: {}, visited europe: {}, meters walked: {}",
+        person.first_name,
+        person.last_name,
+        person.birth_year,
+        person.birth_month,
+        person.visited_europe,
+        person.meter_walked
+    )
 }
